efdb476d385d796d9dafdb5d7af350617cc29e91
FoggyFinder <FoggyFinder@yandex.com>
2023-11-29 18:43:24 +0200

    add SkiaSharp package

--- ViewController.cs
+++ ViewController.cs
@@ -1,4 +1,6 @@
 ﻿using ObjCRuntime;
+using SkiaSharp.Views.Mac;
+using SkiaSharp;
 
 namespace macOSApp1;
 
@@ -9,14 +11,44 @@ public partial class ViewController : NSViewController {
 		// Do not put any initialization here, use ViewDidLoad instead.
 	}
 
-	public override void ViewDidLoad ()
-	{
-		base.ViewDidLoad ();
+    private SkiaSharp.Views.Mac.SKCanvasView skiaView;
+    public override void ViewDidLoad()
+    {
+        base.ViewDidLoad();
+        if (skiaView is null)
+        {
+            skiaView = new SKCanvasView
+            {
+                Frame = new CGRect(0, 0, 600, 400)
+            };
+            this.View = skiaView;
+        }
+        skiaView.IgnorePixelScaling = true;
+        skiaView.PaintSurface += OnPaintSurface;
+    }
 
-		// Do any additional setup after loading the view.
-	}
+    private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+    {
+        // the the canvas and properties
+        var canvas = e.Surface.Canvas;
 
-	public override NSObject RepresentedObject {
+        // make sure the canvas is blank
+        canvas.Clear(SKColors.White);
+
+        // draw some text
+        var paint = new SKPaint
+        {
+            Color = SKColors.Black,
+            IsAntialias = true,
+            Style = SKPaintStyle.Fill,
+            TextAlign = SKTextAlign.Center,
+            TextSize = 24
+        };
+        var coord = new SKPoint(e.Info.Width / 2, (e.Info.Height + paint.TextSize) / 2);
+        canvas.DrawText("SkiaSharp", coord, paint);
+    }
+
+    public override NSObject RepresentedObject {
 		get => base.RepresentedObject;
 		set {
 			base.RepresentedObject = value;
